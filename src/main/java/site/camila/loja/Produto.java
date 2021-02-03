c9f8ec384c296e7df520ffd0ca7b7c3c611e3462
cami <camiladsantoscavalcante@gmail.com>
2021-02-03 19:35:48 -0300

    util class 05.02 - plugins

--- src/main/java/site/camila/loja/Produto.java
+++ src/main/java/site/camila/loja/Produto.java
@@ -1,5 +1,22 @@
 package site.camila.loja;
 
-public class Produto {
+import java.math.BigDecimal;
 
+public class Produto {
+	
+	private String nome;
+	private BigDecimal valor;
+
+	public Produto(String nome, BigDecimal valor) {
+		this.nome = nome;
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
 }
--- src/test/java/site/camila/loja/ProdutoTest.java
+++ src/test/java/site/camila/loja/ProdutoTest.java
@@ -2,13 +2,20 @@ package site.camila.loja;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+
+import org.junit.Assert;
 import org.junit.Test;
+
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		Produto produto = new Produto("teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", produto.getNome());
+		Assert.assertEquals(BigDecimal.TEN, produto.getValor());
+		
 	}
 
 }
